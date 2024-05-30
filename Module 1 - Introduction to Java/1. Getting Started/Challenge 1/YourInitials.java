431fcad9f98db635692c31c931e713b72c9b303d
Lucas Berto <lucaspb.u2@gmail.com>
2024-05-30 18:16:30 -0300

    Adding files to repo

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("L       BBBB ");
+         System.out.println("L       B   B");
+         System.out.println("L       B   B");
+         System.out.println("L       BBBBB");
+         System.out.println("L       B   B");
+         System.out.println("L       B   B");
+         System.out.println("LLLLLL  BBBB");
+        }
+    }
